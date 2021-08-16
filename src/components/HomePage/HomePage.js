51088364446f61ef3992dcb637857cf79d3b4f71
mdsymoon <mdsymoon11@gmail.com>
2021-08-15 23:58:36 -0700

    background-img

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomePage.css'
+const HomePage = () => {
+    return (
+        <div>
+            <div className="img-area">
+            
+            </div>
+            <div className="elements-area">
+                <h1>Beast Products</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae, corrupti!</p>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
